33f39ea0398be2c6ebc5a90381c922ef8a5fad20
mrobbins5 <mrobbins5@wisc.edu>
2018-04-21 15:19:06 -0500

    Add files via upload
    input

---
+++ ram_input_unit.sv
@@ -0,0 +1,22 @@
+module ram_output_unit (
+ input [(DATA_WIDTH-1):0] data,
+ input [(ADDR_WIDTH-1):0] addr,
+ input we, clk,
+ output [(DATA_WIDTH-1):0] q);
+ // Declare the RAM variable
+ reg [DATA_WIDTH-1:0] ram[2**ADDR_WIDTH-1:0];
+ // Variable to hold the registered read address
+ reg [ADDR_WIDTH-1:0] addr_reg;
+
+ initial
+ readmemh("ram_input_contents.txt", ram);
+ end
+
+ always @ (posedge clk)
+ begin
+ if (we) // Write
+ ram[addr] <= data;
+ addr_reg <= addr;
+ end
+ assign q = ram[addr_reg];
+endmodule
