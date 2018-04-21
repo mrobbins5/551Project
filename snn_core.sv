fdc8e6f9dc0db6322e4a3cb5af4921d114deb2dd
mrobbins5 <mrobbins5@wisc.edu>
2018-04-21 14:59:55 -0500

    Add files via upload
    Added instantiations for all the sub-units

--- snn_core.sv
+++ snn_core.sv
@@ -4,7 +4,7 @@
 //SOME MUXES CONNECTING THEM
 //WEIGHTS and LUT ARE PROVIDED 
 
-module snn_core(clk, rst_n, start, d_input, addr_input_unit, digit, done); 
+module snn_core(clk, rst_n, start, q_input, addr_input_unit, digit, done); 
 
 input logic start; 
 input logic d_input; 
@@ -23,6 +23,22 @@ state_t cur_state, nxt_state;
 
 
 //Instantiate MAC module 
+mac mac1(acc, in1, in2, clr, clk, rst_n);
+
+//Instantiate rom_hidden_weight
+rom_hidden_weight rhw1(addr, clk, q);
+ 
+//Instantiate rom_output_weight 
+rom_output_weight row1(addr, clk, q);
+
+//Instantiate ram_hidden_unit
+ram_hidden_unit rhu1(data, addr, we, clk, q); 
+
+//Instantiate ram_output_unit
+ram_output_unit rou1(data, addr, we, clk, q); 
+
+//Instantiate rom_act_func_lut
+rom_act_func_lut rafl(addr, clk, q); 
 
 //SEQUENTIAL LOGIC/////////////////////////////////////////////////////////////////////////////
 
