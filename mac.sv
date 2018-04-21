b49069c23f8cd92591ab2c2f21eee166f70a876a
mrobbins5 <mrobbins5@wisc.edu>
2018-04-21 13:57:50 -0500

    Add files via upload

--- mac.sv
+++ mac.sv
@@ -1,16 +1,15 @@
 //Signed 8-bit MAC module
 
-module mac(of, uf, acc, a, b, clr_n, clk, rst_n);  //Declare all inputs and outputs of the Multiply Accumulator module
+module mac(acc, a, b, clr_n, clk, rst_n);  //Declare all inputs and outputs of the Multiply Accumulator module
 
 //a and b are the 8bit inputs that will be accumulated with outputs previous value
 input signed [7:0] a; 
 input signed [7:0] b; 
 input clr_n, clk, rst_n; 
 
-output logic signed[15:0] acc; //The output is larger due to multiplication
-output logic of, uf; //Logic to show overflow and underlflow
+output logic signed[25:0] acc; //The output is larger due to multiplication
 
-logic [15:0] acc_nxt, add, mult, testACC, testMULT, testSUM;  //Internal wires used to combine and calculate into outputs
+logic [25:0] acc_nxt, add, mult, testACC, testMULT, testSUM;  //Internal wires used to combine and calculate into outputs
 
 //Registers -> Behavioral
 
@@ -20,29 +19,11 @@ always_ff @(posedge clk, negedge rst_n) begin
 
 	//Bottom register
 	if(!rst_n) begin
-		acc <= 16'h0000;
-		of <= 1'b0; 
-		uf <= 1'b0; 
+		acc <= 26'h0000000;
 	
 	end 
 	else begin
-	
 		acc <= acc_nxt;	
-
-		//Overflow or underflow register
-		if(testSUM[15:14] == 2'b01) begin
-                 of <= 1'b1; 
-                 uf <= 1'b0;
-                end
-		
-                else if(testSUM[15:14] == 2'b10) begin
-                 uf <= 1'b1;
-                 of <= 1'b0;
-                 end 
-		else begin
-			of <= 1'b0;
-			uf <= 1'b0;
-		end 
 	end
 	
 end
@@ -53,11 +34,11 @@ end
 assign testMULT = {mult[15], mult[15:0]};
 
 //Sign extend the first bit of accumulator
-assign testACC = {acc[15], acc[15:0]};
+assign testACC = {acc[25], acc[25:0]};
 
 //Sum and compare
 assign testSUM = testMULT + testACC; //
-assign acc_nxt = (clr_n) ? (mult + acc) : 16'h0000;   //Sets the next value of transition (If not reset) to be equalt to mult+acc 
+assign acc_nxt = (clr_n) ? (mult + acc) : 26'h0000000;   //Sets the next value of transition (If not reset) to be equalt to mult+acc 
 assign mult = a*b; 
 		
 
