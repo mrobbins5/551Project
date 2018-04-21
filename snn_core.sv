c2c45c3db9d569607bfa5c7ebabcee536d5515d6
mrobbins5 <mrobbins5@wisc.edu>
2018-04-21 13:40:27 -0500

    Add files via upload

--- snn_core.sv
+++ snn_core.sv
@@ -16,12 +16,13 @@ output logic done;
 
 //FSM Design modeled by professor's diagram/// BP = "Back Porch"
 typedef enum [3:0]{IDLE, MAC_HIDDEN, MAC_HIDDEN_BP1, MAC_HIDDEN_BP2, MAC_HIDDEN_WRITE, 
-					MAC_OUTPUT, MAC_OUTPUT_BPI, MAC_OUTPUT_BP2, MAC_OUPUT_WRITE, DONE} state_t; 
+					MAC_OUTPUT, MAC_OUTPUT_BP1, MAC_OUTPUT_BP2, MAC_OUTPUT_WRITE, DONE} state_t; 
 
 state_t cur_state, nxt_state; 
 
 
-//Step 1: start is asserted by snn
+
+//Instantiate MAC module 
 
 //SEQUENTIAL LOGIC/////////////////////////////////////////////////////////////////////////////
 
@@ -39,6 +40,40 @@ always_comb begin
 	done = 0; 
 	digit = 4'b0000; 
 	addr_input_unit = 10'b00_0000_0000;
+	
+	case(cur_state)
+	IDLE : begin
+	
+	end
+	MAC_HIDDEN : begin
+	
+	end
+	MAC_HIDDEN_BP1 : begin
+	
+	end
+	MAC_HIDDEN_BP2 : begin
+	
+	end
+	MAC_HIDDEN_WRITE : begin
+	
+	end 
+	MAC_OUTPUT : begin
+	
+	end
+	MAC_OUTPUT_BP1 : begin
+	
+	end
+	MAC_OUTPUT_BP2 : begin
+	
+	end
+	MAC_OUTPUT_WRITE : begin
+	
+	end
+	DONE : begin
+	
+	end
+	
+		
 
 end
 
