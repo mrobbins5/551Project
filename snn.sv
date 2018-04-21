839aa38fda864e9d639048c9a7b1dd84cb124a9f
mrobbins5 <mrobbins5@wisc.edu>
2018-04-21 16:17:53 -0500

    Add files via upload
    End of group work 4/21/2018

--- snn.sv
+++ snn.sv
@@ -15,8 +15,9 @@ module SNN(clk, sys_rst_n, led, uart_tx, uart_rx);
 	Reset synchronizer
 	******************************************************/
 	rst_synch i_rst_synch(.clk(clk), .sys_rst_n(sys_rst_n), .rst_n(rst_n));
-	ram_input_unit riu1(data, addr, we, clk, q); 
-	
+	snn_core sc(clk, rst_n, start, q_input, addr_input_unit, digit, done); 
+	ram_input_unit riu1(data, addr, we, clk, q_input); 
+
 	/******************************************************
 	UART
 	******************************************************/
--- snn_core.sv
+++ snn_core.sv
@@ -14,6 +14,7 @@ output logic [9:0] addr_input_unit;
 output logic [3:0] digit; 
 output logic done; 
 
+logic signed [7:0] q_ext; 
 //FSM Design modeled by professor's diagram/// BP = "Back Porch"
 typedef enum [3:0]{IDLE, MAC_HIDDEN, MAC_HIDDEN_BP1, MAC_HIDDEN_BP2, MAC_HIDDEN_WRITE, 
 					MAC_OUTPUT, MAC_OUTPUT_BP1, MAC_OUTPUT_BP2, MAC_OUTPUT_WRITE, DONE} state_t; 
@@ -25,27 +26,38 @@ state_t cur_state, nxt_state;
 //Instantiate MAC module 
 mac mac1(acc, in1, in2, clr, clk, rst_n);
 
-//***************ROM*******************//
+//rect // address TODO
+assign rect_addr = (acc[25] == 0 && |acc[24:17] ) ? 11'h3FF :
+		(acc[25] == 1 && &acc[24:17]) ? 11'h400 : acc [17:7];
+assign addr = rect_addr + 11'h400; 
+		
+		
+//************************************ROM***************************************//
+//////////////////////////////////////////////////////////////////////////////////
 
 //Instantiate rom_hidden_weight
-rom_hidden_weight rhw1(addr, clk, q);
+rom_hidden_weight rhw1(addr, clk, q1);
  
 //Instantiate rom_output_weight 
-rom_output_weight row1(addr, clk, q);
+rom_output_weight row1(addr, clk, q2);
 
 //Instantiate rom_act_func_lut
-rom_act_func_lut rafl(addr, clk, q); 
+rom_act_func_lut rafl(addr, clk, q_lut); 
 
-//***************RAM*******************//
+//*************************************RAM**************************************//
+//////////////////////////////////////////////////////////////////////////////////
 
 //Instantiate ram_hidden_unit
-ram_hidden_unit rhu1(data, addr, we, clk, q); 
+ram_hidden_unit rhu1(data, addr, we, clk, q_weight_hidden); 
 
 //Instantiate ram_output_unit
-ram_output_unit rou1(data, addr, we, clk, q); 
+ram_output_unit rou1(data, addr, we, clk, q_weight_output); 
 
-//Instantiate ram_input_unit
-ram_input_unit riu1(data, addr, we, clk, q); 
+
+//Extend 1-bit q_input to 8-bit to make it either 0 (8’b00000000) or 127 (8’b01111111).
+assign q_ext = (q_input) ? 8'h7F : 8'h0 ;
+
+// assign addr_hidden_weight = (some flag from FSM) addr_hidden_weight + 1 : addr_hidden_weight;
 
 //SEQUENTIAL LOGIC/////////////////////////////////////////////////////////////////////////////
 
@@ -66,10 +78,12 @@ always_comb begin
 	
 	case(cur_state)
 	IDLE : begin
-	
+		//Wait until "start"
+		if(start) nxt_state = MAC_HIDDEN; 
+		else nxt_state = IDLE; 
 	end
 	MAC_HIDDEN : begin
-	
+		//if(something<32) nxt_state = MAC_HIDDEN; 
 	end
 	MAC_HIDDEN_BP1 : begin
 	
@@ -94,6 +108,10 @@ always_comb begin
 	end
 	DONE : begin
 	
+	end
+	//If we go out of bounds
+	default : begin
+		nxt_state = IDLE; 
 	end
 	
 		
