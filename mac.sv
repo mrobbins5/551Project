fa45f95006158b9691b75383734d0c276edd8e3d
mrobbins5 <mrobbins5@wisc.edu>
2018-04-21 14:41:12 -0500

    Add files via upload
    FINAL FINAL VERSION

--- mac.sv
+++ mac.sv
@@ -7,32 +7,26 @@ input signed [7:0] in1;
 input signed [7:0] in2; 
 input clr, clk, rst_n; 
 
-output logic signed[25:0] acc; //The output is larger due to multiplication
+output logic signed [25:0] acc; //The output is larger due to multiplication
 
-logic signed[25:0] acc_nxt, add, mult_ext;  //Internal wires used to combine and calculate into outputs
-logic signed[15:0] mult; 
+logic signed [25:0] acc_nxt, add, mult_ext;  //Internal wires used to combine and calculate into outputs
+logic signed [15:0] mult; 
 
 
 //asynchronous flipflop to implement the transition logic
 always_ff @(posedge clk, negedge rst_n) begin
 
 	//Bottom register
-	if(!rst_n) begin
-		acc <= 26'h0000000;
-	
-	end 
-	else begin
+	if(!rst_n)
+		acc <= 26'h0;
+	else
 		acc <= acc_nxt;	
-	end
-	
 end
 
-//Datapath -> Dataflow
+assign mult_ext = {{10{mult[15]}}, mult[15:0]};
 
-assign mult_ext = {10{mult[15]}, mult[15:0]};
-
-assign acc_nxt = (clr) ? (mult + acc) : 26'h0000000;   //Sets the next value of transition (If not reset) to be equalt to mult+acc 
-assign mult = a*b; 
+assign acc_nxt = (clr) ? (mult_ext + acc) : 26'b0;   //Sets the next value of transition (If not reset) to be equalt to mult+acc 
+assign mult = in1*in2; 
 		
 
 endmodule
