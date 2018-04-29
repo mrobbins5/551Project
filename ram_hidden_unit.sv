054f50c175ef28f2f20b4bed5664100d40aa4f9c
Anapat_C <chairithinug@wisc.edu>
2018-04-29 17:50:54 -0500

    Revert to correct files! DO NOT OVERWRITE!

--- ram_hidden_unit.sv
+++ ram_hidden_unit.sv
@@ -1,25 +1,24 @@
-localparam DATA_WIDTH = 4'h8; 
-localparam ADDR_WIDTH = 3'h5; 
+module ram_hidden_unit #(
+parameter DATA_WIDTH = 4'h8 ,parameter ADDR_WIDTH = 3'h5)(
+input [(DATA_WIDTH-1'b1):0] data,
+input [(ADDR_WIDTH-1'b1):0] addr,
+input we, clk,
+output [(DATA_WIDTH-1'b1):0] q);
 
-module ram_hidden_unit (
- input [(DATA_WIDTH-1):0] data,
- input [(ADDR_WIDTH-1):0] addr,
- input we, clk,
- output [(DATA_WIDTH-1):0] q);
- // Declare the RAM variable
- reg [DATA_WIDTH-1:0] ram[2**ADDR_WIDTH-1:0];
- // Variable to hold the registered read address
- reg [ADDR_WIDTH-1:0] addr_reg;
+// Declare the RAM variable
+reg [DATA_WIDTH-1'b1:0] ram[2**ADDR_WIDTH-1'b1:0];
 
- initial begin
- $readmemh("ram_hidden_contents.txt", ram);
- end
+// Variable to hold the registered read address
+reg [ADDR_WIDTH-1'b1:0] addr_reg;
 
- always @ (posedge clk)
- begin
- if (we) // Write
- ram[addr] <= data;
- addr_reg <= addr;
- end
- assign q = ram[addr_reg];
+initial
+	$readmemh("ram_hidden_contents.txt", ram);
+
+always @ (posedge clk) begin
+	if (we) ram[addr] <= data; // Write
+	addr_reg <= addr;
+end
+
+assign q = ram[addr_reg];
+
 endmodule
