e21e39e2b465d73a746f3e7a0accfe039c65714f
mrobbins5 <mrobbins5@wisc.edu>
2018-04-29 17:54:06 -0500

    Add files via upload
    Updated SNN Core with increment values

--- snn_core.sv
+++ snn_core.sv
@@ -11,6 +11,10 @@ input logic q_input;
 input logic clk, rst_n; 
 
 output logic [9:0] addr_input_unit; 
+logic [14:0] addr_hidden_weight;
+logic [4:0] addr_hidden_unit;
+logic [8:0] addr_output_weight;
+logic [3:0] addr_output_unit;
 output logic [3:0] digit; 
 output logic done; 
 
@@ -27,16 +31,12 @@ logic mac_clr; 							//internal clear for mac
 //Address
 logic [10:0] addr; 
 
-logic addr_input_unit_inc,addr_hidden_weight_inc,addr_hidden_unit_inc,addr_output_weight_inc;
-
-
 logic signed [7:0] q_ext; 
 //FSM Design modeled by professor's diagram/// BP = "Back Porch"
 typedef enum logic [3:0] {IDLE, MAC_HIDDEN, MAC_HIDDEN_BP1, MAC_HIDDEN_BP2, MAC_HIDDEN_WRITE, 
 					MAC_OUTPUT, MAC_OUTPUT_BP1, MAC_OUTPUT_BP2, MAC_OUTPUT_WRITE, DONE} state_t; 
 
 state_t cur_state, nxt_state; 
- 
 
 //////////////////////////////Instantiate MAC module////////////////////////////// 
 mac mac1(acc, in1, in2, mac_clr, clk, rst_n);
@@ -44,23 +44,30 @@ mac mac1(acc, in1, in2, mac_clr, clk, rst_n);
 //************************************ROM***************************************//
 //////////////////////////////////////////////////////////////////////////////////
 
+logic [7:0] q_weight_hidden;
+
 //Instantiate rom_hidden_weight
 rom_hidden_weight rhw1(addr_hidden_weight, clk, q_weight_hidden);
+
+logic [7:0] q_weight_output;
  
 //Instantiate rom_output_weight 
 rom_output_weight row1(addr_output_weight, clk, q_weight_output);
 
+logic [7:0] q_lut;
 //Instantiate rom_act_func_lut
 rom_act_func_lut rafl(addr, clk, q_lut); 
 
 //*************************************RAM**************************************//
 //////////////////////////////////////////////////////////////////////////////////
 
+logic [7:0] d_hidden_unit, q_hidden_unit;
 //Instantiate ram_hidden_unit
-ram_hidden_unit rhu1(d_hidden_unit, addr_hidden_unit, we, clk, q_hidden_unit); 
+ram_hidden_unit rhu1(d_hidden_unit, addr_hidden_unit, we_ram_hidden_unit, clk, q_hidden_unit); 
 
+logic [7:0] d_output_unit, q_unit_output;
 //Instantiate ram_output_unit
-ram_output_unit rou1(d_output_unit, addr, we, clk, q_weight_output); 
+ram_output_unit rou1(d_output_unit, addr_output_unit, we_ram_output_unit, clk, q_unit_output); 
 
 //Extend 1-bit q_input to 8-bit to make it either 0 (8’b00000000) or 127 (8’b01111111).
 assign q_ext = (q_input) ? 8'h7F : 8'h0 ;
@@ -97,15 +104,54 @@ assign rect_addr = (acc[25] == 0 && |acc[24:17] ) ? 11'h3FF :
 assign addr = rect_addr + 11'h400; 
 
 /////////////////////////////////ASSIGN OUTPUTS///////////////////////////////////
-assign digit = q_weight_output; 
+assign digit = q_unit_output; 
 assign done = (doneFlag) ? 1'b1 : 1'b0; 
 
-assign addr_input_unit = (addr_input_unit_inc) ? addr_input_unit + 1'b1 : addr_input_unit;
-assign addr_hidden_weight = (addr_hidden_weight_inc) ? addr_hidden_weight + 1'b1 : addr_hidden_weight;
-assign addr_hidden_unit = (addr_hidden_unit_inc) ? addr_hidden_unit + 1'b1 : addr_hidden_unit;
-assign addr_output_weight = (addr_output_weight_inc) ? addr_output_weight + 1'b1 : addr_output_weight;
-// addr_input_unit control RAM_INPUT_UNIT_Address
-// and increment every time it gets a bit
+
+/////////////////////////////////ADDR ASSIGNMENT//////////////////////////////////
+logic addr_output_weight_clr, addr_output_weight_inc; 	//output weight
+logic addr_input_unit_clr, addr_input_unit_inc;			//input unit
+logic addr_input_inc, addr_input_clr;					//input addr
+logic addr_hidden_weight_inc, addr_hidden_weight_clr;	//hidden weight
+logic addr_hidden_unit_inc, addr_hidden_unit_clr;		//hidden unit
+logic addr_output_unit_inc,addr_output_unit_clr;		//output unit
+
+always_ff @ (posedge clk, negedge rst_n) begin
+if (!rst_n) begin
+	addr_input_unit <= 10'b0;
+	addr_hidden_weight <= 16'b0;
+	addr_hidden_unit <= 5'b0;
+	addr_output_weight <= 9'b0;
+	addr_output_unit <= 4'b0;
+	end
+else begin
+	if (addr_input_unit_clr)
+		addr_input_unit <= 10'b0;
+	else if (addr_input_unit_inc)
+		addr_input_unit <= addr_input_unit + 1'b1;
+	
+	if (addr_hidden_weight_clr)
+		addr_hidden_weight <= 10'b0;
+	else if (addr_hidden_weight_inc)
+		addr_hidden_weight <= addr_hidden_weight + 1'b1;
+	
+	if (addr_hidden_unit_clr)
+		addr_hidden_unit <= 10'b0;
+	else if (addr_hidden_unit_inc)
+		addr_hidden_unit <= addr_hidden_unit + 1'b1;
+	
+	if (addr_output_weight_clr) 
+		addr_output_weight <= 10'b0;
+	else if (addr_output_weight_inc)
+		addr_output_weight <= addr_output_weight + 1'b1;
+		
+	if (addr_output_unit_clr) 
+		addr_output_unit <= 10'b0;
+	else if (addr_output_unit_inc)
+		addr_output_unit <= addr_output_unit + 1'b1;
+end
+end
+///////////////////////////////////////////////////////////////////////////////////////////////
 
 ////////////////////////////////////////COMBINATIONAL LOGIC////////////////////////////////////
 always_comb begin
@@ -115,9 +161,13 @@ always_comb begin
 	mac_clr = 1'b0; 
 	doneFlag = 1'b0; 
 	addr_input_unit_inc = 1'b0;
+	addr_input_unit_clr = 1'b0;
 	addr_hidden_weight_inc = 1'b0;
+	addr_hidden_weight_clr = 1'b0;
 	addr_hidden_unit_inc = 1'b0;
+	addr_hidden_unit_clr = 1'b0;
 	addr_output_weight_inc = 1'b0;
+	addr_output_weight_clr = 1'b0;
 	macIn1Sel = 1'b0; 
 	macIn2Sel = 1'b0; 
 	
@@ -125,6 +175,9 @@ always_comb begin
 	IDLE : begin
 		//Wait until "start"
 		clear784Flag = 1'b1; //Set clear 784 flag
+		addr_input_unit_clr = 1'b1;
+		addr_hidden_weight_clr = 1'b1;
+		addr_hidden_unit_clr = 1'b1;
 		macIn1Sel = 1'b0; 
 		macIn2Sel = 1'b0; 
 		if(start) nxt_state = MAC_HIDDEN; 
@@ -136,14 +189,16 @@ always_comb begin
 		//If we haven't counted to 784, stay here
 		if(count784 != 12'h310) begin
 			nxt_state = MAC_HIDDEN; 
-			addr_input_unit_inc = 1'b1;		//Start incrementing input address
-			addr_hidden_weight_inc = 1'b1;	//Start incrementing hidden address
+			addr_input_unit_inc = 1'b1;
+			addr_hidden_weight_inc = 1'b1;
 		end
 		
 		//Continue to next state
 		else begin
 			nxt_state = MAC_HIDDEN_BP1; 
-			addr_hidden_weight_inc = 1'b0; 	//Stop incrementing hidden address for porches
+			addr_input_unit_inc = 1'b0; 
+			addr_hidden_weight_inc = 1'b0; 
+			
 		end
 	end
 	
@@ -166,14 +221,17 @@ always_comb begin
 		//Haven't finished all 784 bits 
 		if(count32 != 6'h20) begin
 			nxt_state = MAC_HIDDEN;
-			addr_hidden_weight_inc = 1'b1; //Start incrementing hidden address after porches
+			addr_input_unit_inc = 1'b1;
+			addr_hidden_weight_inc = 1'b1;
 			clear784Flag = 1'b1; 
 			mac_clr = 1'b1; 
 		end
 		//Finished all 32 bits
 		else begin
 			nxt_state = MAC_OUTPUT;
-			addr_output_weight_inc = 1'b1; //Start incrementing ouput address
+			addr_input_unit_inc = 1'b0; 
+			addr_hidden_unit_inc = 1'b0; 
+			addr_output_unit_inc = 1'b1; 
 			clear32Flag = 1'b1; 
 			macIn2Sel = 1'b1; 
 			macIn1Sel = 1'b1; 
@@ -182,10 +240,11 @@ always_comb begin
 	end 
 	
 	MAC_OUTPUT : begin
+		addr_output_weight_inc = 1'b1; 
 		if(count32 != 6'h20) nxt_state = MAC_OUTPUT; 
 		else begin
 			nxt_state = MAC_OUTPUT_BP1; 
-			addr_output_weight_inc = 1'b0; //Stop incrementing output address
+			addr_output_weight_inc = 1'b0; 
 		end
 	end
 	
@@ -199,11 +258,12 @@ always_comb begin
 	end
 	
 	MAC_OUTPUT_WRITE : begin
+		addr_output_weight_inc = 1'b1; 
 		if(count784 != 6'h0A) begin
 			nxt_state = MAC_OUTPUT; //We reuse 784 counter to count to 10
-			addr_output_weight_inc = 1'b1; //Start incrementing ouput address again
 			clear32Flag = 1'b1; 
 			mac_clr = 1'b1; 
+			addr_output_unit_inc = 1'b1; 
 		end
 		else begin 
 			nxt_state = DONE; 
@@ -213,6 +273,8 @@ always_comb begin
 	DONE : begin
 		nxt_state = IDLE; 
 		doneFlag = 1'b1; 
+		
+		digit=
 	end
 	
 	//If we go out of bounds
