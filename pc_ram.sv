075017d97dd52044ed980be48df2d7a8d1fc9016
mrobbins5 <mrobbins5@wisc.edu>
2018-05-03 16:39:41 -0500

    Some guy forgot to upload this

---
+++ pc_ram.sv
@@ -0,0 +1,23 @@
+module pc_ram #(
+parameter DATA_WIDTH = 1'b1, parameter ADDR_WIDTH = 4'ha)(
+input [(DATA_WIDTH-1'b1):0] data,
+input [(ADDR_WIDTH-1'b1):0] addr,
+input we, clk,
+output [(DATA_WIDTH-1'b1):0] q);
+
+// Declare the RAM variable
+reg [DATA_WIDTH-1'b1:0] ram[2**ADDR_WIDTH-1'b1:0];
+
+// Variable to hold the registered read address
+reg [ADDR_WIDTH-1'b1:0] addr_reg;
+
+initial $readmemb("ram_input_contents_sample_2.txt", ram);
+
+always @ (posedge clk) begin
+	if (we) ram[addr] <= data; // Write
+	addr_reg <= addr;
+end
+
+assign q = ram[addr_reg];
+
+endmodule
